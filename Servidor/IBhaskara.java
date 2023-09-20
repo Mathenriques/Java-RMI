964bd8cc6679e31ec62b7e251c9e0cbc4c872a54
Mathenriques <math.marqui@gmail.com>
2023-09-20 20:26:59 -0300

    Ajuste de formatação

--- Servidor/IBhaskara.java
+++ Servidor/IBhaskara.java
@@ -1,6 +1,6 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface IBhaskara extends Remote{
+public interface IBhaskara extends Remote {
   String calcBhaskara(double a, double b, double c) throws RemoteException;
 }
